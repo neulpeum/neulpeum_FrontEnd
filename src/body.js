b39ef03caa0ce7c3b19ede63a4d4a3f5c8e1f393
buna <tmdgh1592@naver.com>
2024-01-21 23:34:51 +0900

    feat: 프로젝트 기초 세팅

---
+++ src/body.js
@@ -0,0 +1,11 @@
+function createBody() {
+    return (
+        <div>
+            <header>
+                <h1>본문 부분!!</h1>
+            </header>
+        </div>
+    );
+}
+
+export default createBody;
---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import logo from './ic_logo.svg';
+import home from './ic_home.svg';
+import './Header.css';
+
+function createHeader() {
+  return(
+    <div class="header-container">
+      <div class="logo-container">
+        <img src={logo} alt="logo" class="logo"/>
+      </div>
+
+      <a href="#" class="home-button">
+          <img src={home} alt="Home"/>
+      </a>
+    </div>
+  )
+}
+
+export default createHeader;
