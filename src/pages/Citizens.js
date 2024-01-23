38d9857df85e9124db175390e8631881afac2671
buna <tmdgh1592@naver.com>
2024-01-23 12:18:25 +0900

    feat: Citizens 페이지에 router-dom 적용

---
+++ src/pages/Citizens.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Citizens = () => {
+  return (
+    <div>
+      <h2>Citizens</h2>
+      <p>This is the content of the subpage.</p>
+    </div>
+  );
+};
+
+export default Citizens;
