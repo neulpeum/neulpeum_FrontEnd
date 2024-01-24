54e58853e0fa0c1434d1866c4e04c45e6193697a
buna <tmdgh1592@naver.com>
2024-01-24 21:52:19 +0900

    feat: SearchBar img태그에 alt 속성 추가

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -9,15 +9,15 @@ const SearchBar = () => {
     <div className="search-bar-container">
       <div className="search-input-container">
         <input type="text" placeholder="검색할 주민의 이름을 입력하세요."/>
-        <img src={search} className="search-icon"/>
+        <img src={search} className="search-icon" alt="검색"/>
       </div>
 
       <div className="sort-container">
         <p>역방향 정렬</p>
-        <img src={sort} className="sort-icon"/>
+        <img src={sort} className="sort-icon" alt="정렬"/>
       </div>
 
-      <img src={plus} className="plus-icon"/>
+      <img src={plus} className="plus-icon" art="주민 추가"/>
     </div>
   );
 };
