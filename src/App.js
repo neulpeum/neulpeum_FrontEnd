f702b5d71af0779e89cc5c8dd5b092186025f5b4
yxoni <90109410+yxoni@users.noreply.github.com>
2024-01-27 22:44:05 +0900

    Feat: init CitizensDatails

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 import HeaderComponent from "./components/header/Header";
 import { Routes, Route } from "react-router-dom";
-import Citizens from './pages/Citizens';
+import { Link } from "react-router-dom";
+import Citizens from "./pages/Citizens";
+import CitizensDetails from "./pages/CitizensDetails";
 
 const App = () => {
   return (
     <div className="app">
-      <HeaderComponent/>
+      <HeaderComponent />
       <Routes>
-        <Route path="/citizens" element={<Citizens />} />
-      </Routes>
+        <Route path="/citizens" element={<Citizens />} />{" "}
+        <Route path="/citizensDetails" element={<CitizensDetails />} />{" "}
+      </Routes>{" "}
+      <Link to="/citizensDetails"> Go to Individual Page </Link>{" "}
     </div>
   );
 };
---
+++ src/pages/CitizensDetails.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CitizensDetails = () => {
+  return <div> CitizensDetails </div>;
+};
+
+export default CitizensDetails;
