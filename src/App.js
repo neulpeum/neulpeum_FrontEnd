896296a1395db311fbb867df4995b89add6db75b
hyong0117 <103429879+dhxogud@users.noreply.github.com>
2024-02-22 20:54:54 +0900

    충돌 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
-
-import Drugs from "./pages/drugs/Drugs";
-import Citizens from "./pages/citizens/Citizens";
-import CitizensDetails from "./pages/citizensDetails/CitizensDetails";
-import CitizenAdd from "./components/citizenAdd/CitizenAdd";
-import Main from './pages/main/Main';
-import AccountSetting from './pages/accountSetting/AccountSetting';
-import AddCounsel from './pages/addCounsel/Addcounsel';
-import Options from './pages/options/Options';
 
 const App = () => {
   return (
