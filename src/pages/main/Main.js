2bf5ddf22cc4534078f831bc66b1c3e721def8b4
hyong0117 <103429879+dhxogud@users.noreply.github.com>
2024-04-13 12:57:20 +0900

    2024.4.13.12:57

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 import CustomInput from './CustomInput';
 import LoginButton from './LoginButton';
-import LoginWrapper from 'components/LoginWrapper';
 import 'styles/ForPages/Main/Main.css';
 
 
@@ -35,20 +34,21 @@ const Main = () => {
     }
   }
  
-  const LoginView = 
-  <>
-    <div className="main-logo-container">
+  return (
+    <>
+    <div className="main-container">
+      <div className="main-logo-container">
           <img src="/icons/ic_logo.svg" alt="logo" className="logo" />
         </div>
-    <div className='main-content'>
-      <p>늘픔 약 재고 관리 서비스에 오신 것을 환영합니다</p>
-      <CustomInput inputValue={id} onChange={(event) => setId(event.target.value)} placeholder="이름을 입력해주세요." icon="/icons/ic_name.svg" isPassword={false} onKeyDown={(e) => activeEnter(e)}/>
-      <CustomInput inputValue={password} onChange={(event) => setPassword(event.target.value)} placeholder="비밀번호를 입력해주세요." icon="/icons/ic_password.svg" isPassword={true} onKeyDown={(e) => activeEnter(e)} />
-      <LoginButton onLoginClick={login}/>
+      <div className='main-content'>
+        <p>늘픔 약 재고 관리 서비스에 오신 것을 환영합니다</p>
+        <CustomInput inputValue={id} onChange={(event) => setId(event.target.value)} placeholder="이름을 입력해주세요." icon="/icons/ic_name.svg" isPassword={false} onKeyDown={(e) => activeEnter(e)}/>
+        <CustomInput inputValue={password} onChange={(event) => setPassword(event.target.value)} placeholder="비밀번호를 입력해주세요." icon="/icons/ic_password.svg" isPassword={true} onKeyDown={(e) => activeEnter(e)} />
+        <LoginButton onLoginClick={login}/>
+      </div>
     </div>
-  </>
-  return (
-    <LoginWrapper content={LoginView} />
+    <div className="main-footer"/>
+    </>
   );
 };
 
--- src/pages/options/Options.js
+++ src/pages/options/Options.js
@@ -3,32 +3,32 @@ import MyInfoBtn from 'Images/img_my_info_btn.svg';
 import CitizensBtn from 'Images/img_citizens_btn.svg';
 import MedicineBtn from 'Images/img_medicine_btn.svg';
 import { Link } from "react-router-dom";
-import LoginWrapper from 'components/LoginWrapper';
 import 'styles/ForPages/Options/Options.css';
 
-const View = 
-<>
-<div className="option-logo-container">
-  <img src="/icons/ic_logo.svg" alt="logo" className="logo" />
-</div>
-<div className='main-content'>
-<p>늘픔 약 재고 관리 서비스에 오신 것을 환영합니다</p>
-  <div className="option-btn-container">
-    <Link className="default-link-styles" to="/citizens">
-      <img src={CitizensBtn} alt="citizens-btn" className="citizens-btn" />
-    </Link>
-    <Link className="default-link-styles" to="/drugs">
-      <img src={MedicineBtn} alt="medicine-btn" className="medicine-btn" />
-    </Link>
-    <Link className="default-link-styles" to="/accountSettings">
-      <img src={MyInfoBtn} alt="my-info-btn" className="my-info-btn" />
-    </Link>
-  </div>
-</div>
-</>
-
 const Options = () => {
-    return ( <LoginWrapper content={View}/>
+    return ( 
+      <>
+      <div className="option-container">
+        <div className="option-logo-container">
+          <img src="/icons/ic_logo.svg" alt="logo" className="logo" />
+        </div>
+        <div className='option-content'>
+        <p>늘픔 약 재고 관리 서비스에 오신 것을 환영합니다</p>
+        <div className="option-btn-container">
+          <Link className="default-link-styles" to="/citizens">
+            <img src={CitizensBtn} alt="citizens-btn" className="citizens-btn" />
+          </Link>
+          <Link className="default-link-styles" to="/drugs">
+            <img src={MedicineBtn} alt="medicine-btn" className="medicine-btn" />
+          </Link>
+          <Link className="default-link-styles" to="/accountSettings">
+            <img src={MyInfoBtn} alt="my-info-btn" className="my-info-btn" />
+          </Link>
+        </div>
+        </div>
+      </div>
+      <div className="option-footer"/>
+      </>
     );
   };
 
